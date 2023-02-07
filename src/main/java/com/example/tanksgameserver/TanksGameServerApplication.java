47314db7b449fb05114c70a4887eb55b22818654
Artyom Belsky <artyombel2003@gmail.com>
2023-02-07 22:29:15 +0300

    First commit

---
+++ src/main/java/com/example/tanksgameserver/TanksGameServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tanksgameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanksGameServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TanksGameServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tanksgameserver/TanksGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tanksgameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanksGameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
